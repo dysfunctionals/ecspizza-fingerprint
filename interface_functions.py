d560c749401d73cd5545d0e2e525d27384cea93b
Andrew Barrett-Sprot <abarrettsprot@gmail.com>
2018-03-11 05:26:29 +0000

    fix docstrings

--- interface_functions.py
+++ interface_functions.py
@@ -29,9 +29,12 @@ def get_image_fingerprint(img_name):
 
 def get_pizza_similarity(fingerprint_1, fingerprint_2):
     """
-    Get the similarity of two pizza f
-    :param img_name: image file name to get the data for
-    :return: fingerprint data in the form of a list of floats
+    Get the similarity of two pizza fingerprints.
+    Anything higher than 0.5 can be considered a match.
+
+    :param fingerprint_1: fingerprint 1 to compare against
+    :param fingerprint_2: fingerprint 2 to compare against
+    :return: similarity of a pizza (>0.5 is a match)
     """
     sameness = similarity(fingerprint_1, fingerprint_2)
     certainty = 1-((sameness/18000)*0.5)
